4f09cbddc3919647fd8411b082e2898cc9c7de68
Naomi Liu <naomiliu@Naomis-MacBook-Pro.local>
2021-03-07 12:37:19 -0800

    project

--- File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Naomi Liu on 3/7/21.
-//
-
-import Foundation
---
+++ playmate/playmate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  playmate
+//
+//  Created by Naomi Liu on 3/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
