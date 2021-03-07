24c4f67ff73a126dae1deb041bcfa085871948ac
Naomi Liu <naomiliu@Naomis-MacBook-Pro.local>
2021-03-07 13:34:52 -0800

    login skeleton

--- playmate/playmate/ViewController.swift
+++ playmate/playmate/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var playmateLabel: UIView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
