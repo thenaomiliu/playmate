635156a700c9de28ffaf6786b02b039eac65b36d
Naomi Liu <naomiliu@Naomis-MacBook-Pro.local>
2021-03-07 13:08:08 -0800

    added labels + buttons

--- playmate/playmate/ViewController.swift
+++ playmate/playmate/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var playmateLabel: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginPressed(_ sender: Any) {
+    }
+    
+    @IBAction func createAccPressed(_ sender: Any) {
+    }
 }
 
